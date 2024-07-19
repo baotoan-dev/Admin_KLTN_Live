dc4df931d49e763a2fc856a1cc5c84349350fbf9
Bao Toan <20161385@student.hcmute.edu.vn>
2024-07-19 12:09:23 +0700

    update url

--- src/constant/urlServer.js
+++ src/constant/urlServer.js
@@ -1,9 +1,12 @@
-const API_CONSTANT_V1 = 'https://backend-hcmute-nodejs.onrender.com/api';
+// const API_CONSTANT_V1 = 'https://backend-hcmute-nodejs.onrender.com/api';
 
-const API_CONSTANT_V3 = 'https://backend-hcmute-nestjs.onrender.com/api';
+// const API_CONSTANT_V3 = 'https://backend-hcmute-nestjs.onrender.com/api';
 
 // const API_CONSTANT_V1 = 'http://localhost:8888/api';
 
 // const API_CONSTANT_V3 = 'http://localhost:1902/api';
+const API_CONSTANT_V1 = 'https://welcome-unlimited-summaries-formerly.trycloudflare.com'
+
+const API_CONSTANT_V3 = 'https://lending-advantage-pale-xp.trycloudflare.com'
 
 export { API_CONSTANT_V1, API_CONSTANT_V3 };
